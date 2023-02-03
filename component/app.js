fd26db11ecc547e0f6c3d6cf0e4fcbaed1bf9539
Nicholas-Vandyck <nicholas.vandyck@amalitech.com>
2023-02-03 16:11:05 +0000

    working on JS

---
+++ component/app.js
@@ -0,0 +1,7 @@
+let name = document.getElementById('name')
+let mail = document.getElementById('email')
+let message = document.getElementById('message')
+let erro_message = document.querySelector('alert_message')
+
+
+Form.addEventListener
